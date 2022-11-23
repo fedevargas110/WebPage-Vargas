f95b800c5ee994bd167cfb8220e479a5cd180635
fedevargas110 <fedevargas110@gmail.com>
2022-11-23 20:20:31 -0300

    añadimos scroll smooth

--- JS/script.js
+++ JS/script.js
@@ -3,43 +3,16 @@ document.addEventListener('DOMContentLoaded', function(){
 });
 
 function iniciarApp(){
-    crearGaleria();
     scrollNav();
 }
 
-function crearGaleria(){
-    const galeria = document.querySelector('.galeria-fotos');
+function scrollNav(){
+    const enlace = document.querySelector('.scroll')
+    enlace.addEventListener('click', function(e){
+        e.preventDefault();
 
-    for (let i=1; i<5; i++){
-        const imagen = document.createElement('picture');
-        imagen.innerHTML= `
-            <source srcset= "photos/productos/${i}.webp" type="imagen/webp">
-            
-            <img loading= "lazy" src="photos/productos/${i}.webp" alt="imagen producto">
-        `;
-    }
-}
-
-function scrollNav (){ //Definiendo la funcion de smooth control
-    const enlaces = document.querySelectorAll('.item a');
-    enlaces.forEach(enlace => {
-        enlace.addEventListener('click', function(e){
-            e.preventDefault();//Para prevenir la accion de que te lleve de golpe.
-            const seccionScroll = e.target.attributes.href.value; //Te traiga el href de cada a
-            const seccion = document.querySelector(seccionScroll);
-            seccion.scrollIntoView({behavior:'smooth'}); //Añadimos la animacion smooth
-        });
-    });
-}
-
-function scrollNav (){ //Definiendo la funcion de smooth control
-    const enlaces = document.querySelectorAll('.navegacion a');
-    enlaces.forEach(enlace => {
-        enlace.addEventListener('click', function(e){
-            e.preventDefault();//Para prevenir la accion de que te lleve de golpe.
-            const seccionScroll = e.target.attributes.href.value; //Te traiga el href de cada a
-            const seccion = document.querySelector(seccionScroll);
-            seccion.scrollIntoView({behavior:'smooth'}); //Añadimos la animacion smooth
-        });
+        const seccionScroll = e.target.attributes.href.value;
+        const seccion = document.querySelector(seccionScroll);
+        seccion.scrollIntoView({ behavior: "smooth" });
     });
 }
