25a2c9a910177394b07f35337016f1ce057dd523
fedevargas110 <fedevargas110@gmail.com>
2022-11-23 18:49:20 -0300

    footer productos

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
 function iniciarApp(){
     crearGaleria();
+    scrollNav();
 }
 
 function crearGaleria(){
@@ -17,4 +18,28 @@ function crearGaleria(){
             <img loading= "lazy" src="photos/productos/${i}.webp" alt="imagen producto">
         `;
     }
+}
+
+function scrollNav (){ //Definiendo la funcion de smooth control
+    const enlaces = document.querySelectorAll('.item a');
+    enlaces.forEach(enlace => {
+        enlace.addEventListener('click', function(e){
+            e.preventDefault();//Para prevenir la accion de que te lleve de golpe.
+            const seccionScroll = e.target.attributes.href.value; //Te traiga el href de cada a
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({behavior:'smooth'}); //Añadimos la animacion smooth
+        });
+    });
+}
+
+function scrollNav (){ //Definiendo la funcion de smooth control
+    const enlaces = document.querySelectorAll('.navegacion a');
+    enlaces.forEach(enlace => {
+        enlace.addEventListener('click', function(e){
+            e.preventDefault();//Para prevenir la accion de que te lleve de golpe.
+            const seccionScroll = e.target.attributes.href.value; //Te traiga el href de cada a
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({behavior:'smooth'}); //Añadimos la animacion smooth
+        });
+    });
 }
