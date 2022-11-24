234f2c5352fba02d605ae0ead1cc1e0f0aa2a1a0
fedevargas110 <fedevargas110@gmail.com>
2022-11-23 21:14:00 -0300

    cambiando fotos, agregando con js

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
 function iniciarApp(){
     scrollNav();
+    crearGaleria();
 }
 
 function scrollNav(){
@@ -15,4 +16,20 @@ function scrollNav(){
         const seccion = document.querySelector(seccionScroll);
         seccion.scrollIntoView({ behavior: "smooth" });
     });
+}
+
+function crearGaleria(){
+    const galeria = document.querySelector('.galeria-imagenes');
+    
+    for(let i = 1; i<=12; i++){
+        const imagen = document.createElement('picture');
+        imagen.innerHTML = `
+            <source srcset="photos/productos/${i}.jpg" type="image/jpg">
+            <source srcset="photos/productos/${i}.png" type="image/png">
+            <source srcset="photos/productos/${i}.webp" type="image/webp">
+            <img loading="lazy" width="100" height="100" src="photos/productos/${i}.jpg" alt="imagen productos">
+        
+        `
+        galeria.appendChild(imagen);
+    }
 }
