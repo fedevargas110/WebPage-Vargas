8ea59865a3f273356ab9f7c2b2cb80bd5095b2c8
fedevargas110 <fedevargas110@gmail.com>
2022-11-25 00:20:08 -0300

    Pagina de Productos finalizada?

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     iniciarApp();
 });
 
-function iniciarApp(){
+function iniciarApp() {
     scrollNav();
-    crearGaleria();
+//    crearGaleria();
 }
 
-function scrollNav(){
+function scrollNav() {
     const enlace = document.querySelector('.scroll')
-    enlace.addEventListener('click', function(e){
+    enlace.addEventListener('click', function (e) {
         e.preventDefault();
 
         const seccionScroll = e.target.attributes.href.value;
@@ -18,18 +18,26 @@ function scrollNav(){
     });
 }
 
-function crearGaleria(){
+function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes');
-    
-    for(let i = 1; i<=12; i++){
+
+    for (let i = 1; i <= 12; i++) {
         const imagen = document.createElement('picture');
         imagen.innerHTML = `
             <source srcset="photos/productos/${i}.jpg" type="image/jpg">
             <source srcset="photos/productos/${i}.png" type="image/png">
             <source srcset="photos/productos/${i}.webp" type="image/webp">
-            <img loading="lazy" width="100" height="100" src="photos/productos/${i}.jpg" alt="imagen productos">
-        
+            <img loading="lazy" width="250" height="250" src="photos/productos/${i}.jpg" alt="imagen productos">
         `
         galeria.appendChild(imagen);
+    }
+
+    const nombre = String['Tomate Perita', 'Tomate Redondo', 'Naranja de Jugo', 'Naranja de Ombligo',
+        'Mandarina Murcott', 'Manzana Roja', 'Pera', 'Limon', 'Cebolla', 'Zanahoria', 'Pimiento Verde', 'Pimiento Rojo'];
+    for (let i = nombre; i <= 11; i++) {
+        const nombres = document.createElement('h4');
+        nombres.innerHTML = `
+        <h3>${i}<h3>
+        `
     }
 }
